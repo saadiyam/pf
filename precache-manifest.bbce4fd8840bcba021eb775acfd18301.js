2ccb4819426632a97b15bf2af7da6e0166592341
unknown <itsmesaadiya@gmail.com>
2020-09-21 18:16:47 +0400

    Updates

---
+++ precache-manifest.bbce4fd8840bcba021eb775acfd18301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d20d5d175c6cfa292ea",
+    "url": "/pf/static/css/main.d621506c.chunk.css"
+  },
+  {
+    "revision": "6d20d5d175c6cfa292ea",
+    "url": "/pf/static/js/main.6d20d5d1.chunk.js"
+  },
+  {
+    "revision": "e827e568261231cf74a0",
+    "url": "/pf/static/js/runtime~main.e827e568.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/pf/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "819305193292c59f52b6a215a26c3b97",
+    "url": "/pf/index.html"
+  }
+];
